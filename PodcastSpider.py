7690fb17b707421b089ed6d9eb3490e9586ff274
Anna Evans <40345534+eliannaevans@users.noreply.github.com>
2021-02-20 16:57:39 -0600

    Update PodcastSpider.py
    For Rich Roll podcast, crawl to episode descriptions.

--- PodcastSpider.py
+++ PodcastSpider.py
@@ -5,9 +5,16 @@ class PodcastSpider(scrapy.Spider):
     name = 'podcast_spider'
     
     def start_requests(self):
-        urls=['placeholder.com'] # while a real site, this is a paceholder for podcast site(s)
+        urls=['https://podcasts.google.com/search/rich%20roll'] # start with podcast with known sponsors
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse_main)
             
     def parse_main(self, response):
+        website_body = response.xpath('/html/body/c-wiz[2]')
+        episode_links = response.css('a.jJ8Epb::attr(href)').extract()
+        
+        for link in episode_links:
+            yield response.follow(url=link, callback=self.parse_episodes)
+            
+    def parse_episodes(self, response):
         pass
